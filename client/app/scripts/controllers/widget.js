34e2850643018f41bf79ecb8a1fbb11a1571b5c3
Jeffrey <jeffrey@wix.com>
2014-06-10 21:27:39 -0700

    Added comments into widget controller to clarify functions and variables; fixed some issues with retry and cancel buttons

--- client/app/scripts/controllers/widget.js
+++ client/app/scripts/controllers/widget.js
@@ -2,21 +2,21 @@
 
 angular.module('sendFiles')
   .controller('WidgetCtrl', function ($scope, api, $wix) {
-    $scope.emailRegex = /^[a-z0-9!#$%&'*+/=?^_`{|}~.-]+@[a-z0-9-]+(\.[a-z0-9-]+){1}$/;
+    
+    /* Regular expression used to determine if user input is a valid email. */
+    $scope.emailRegex = /^[A-Za-z0-9!#$%&'*+/=?^_`{|}~.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+){1}$/;
 
+    /* If true, invalid email messages are shown. */
     $scope.showInvalidEmail = false;
 
-    $scope.enableMessage = function() {
-      if ($scope.fileForm.$invalid) {
-        $scope.showInvalidEmail = true;
-      }
-    };
+    /* If true, "no message written" message is shown. */
 
-    $scope.disableMessage = function() {
-      $scope.showInvalidEmail = false;
-    };
+    /* If true, upload failure messages are shown. */
+    $scope.uploadFailed = false;
 
-    //Pseudocode for getting settings to appear
+    /* These variables represent the default text in the widget.
+     * They can be configured by the user, but these are their variable
+     * names used in the widget's HTML file. */
     $scope.headlineText = 'Upload the file and send it to us. We will review it as soon as possible.';
     $scope.addButtonText = '+ Add Files';
     $scope.noFileText = '';
@@ -24,8 +24,25 @@ angular.module('sendFiles')
     $scope.messageText = 'You can add a message to site owner';
     $scope.submitButtonText = 'Submit';
 
-    $scope.email = 'hello';
-    //$scope.cool = false;
+    /* Call this to get error messages to show up if the form
+     * is filled out incorrectly. */
+    $scope.enableErrorMessage = function() {
+      if ($scope.fileForm.email.$invalid) {
+        $scope.showInvalidEmail = true;
+      }
+      if ($scope.fileForm.message.$invalid) {
+        $scope.showNoMessage = true;
+      }
+    };
+
+    /* Call this to get error messages to disappear. */
+    $scope.disableErrorMessage = function() {
+      $scope.showInvalidEmail = false;
+      $scope.showNoMessage = false;
+    };
+
+    /* Call this function after the user has changed their settings
+     * to initiate changes in the widget. */
     $scope.setSettings = function() {
       console.log('hi');
       $wix.Settings.refreshApp();
@@ -33,5 +50,33 @@ angular.module('sendFiles')
       console.log('but');
     };
 
+    /* Call this when the user clicks submit to begin file upload. */
+    $scope.startUpload = function() {
+      // do files need to do be an argument to this function?
+
+      //upload the files 
+      //call backend 
+    };
+
+    /* Call this function when the file has failed to upload. Changes
+     * widget to show error messages to the site visitor and gives
+     * opportunity to try to upload again. */
+    $scope.initiateFailure = function() {
+      $scope.uploadFailed = true;
+    };
+
+    /* Call this to reset widget after widget upload fail.
+     * All upload failure messages will disappear. */
+    $scope.reset = function() {
+      $scope.uploadFailed = false;
+    };
+
+    /* Call this to show success widget after widget upload.
+     * A "Add more files" Button will appear to allow the user
+     * to upload more files. */
+    $scope.success = function() {
+      $scope.headlineText = 'Success! Your files were sent.';
+      $scope.success = true;
+    };
 
   });
